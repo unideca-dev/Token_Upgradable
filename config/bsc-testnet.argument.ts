bad922ff311d8766c02d77922069b9ec0dcfb93c
unideca <unideca177@gmail.com>
2025-05-19 09:59:01 +0900

    add contract verifing sccript and change arguments for contract

--- config/bsc-testnet.argument.ts
+++ config/bsc-testnet.argument.ts
@@ -6,10 +6,10 @@ const WALLET_PK = vars.get("WALLET_PK", "");
 
 const TokenIface = new Interface(TokenUpgradeableABI.abi);
 const initializeData = TokenIface.getFunction('initialize', [WALLET_PK])
-// 0xc4d66de80000000000000000000000002952bee27b5051f48596f5157ac999d7a7792248
+// 0xc4d66de8000000000000000000000000b6f10D81430BEC976276B0AF1930d82B1d1ec117
 // console.log(initializeData);    
 
 export default [
-    "0x65f870B5D8F0c7402B5D8a13E8E2E06Cd86EaA33",
-    "0xc4d66de80000000000000000000000002952bee27b5051f48596f5157ac999d7a7792248",
+    "0xb6f10D81430BEC976276B0AF1930d82B1d1ec117", // implement address
+    "0xc4d66de80000000000000000000000002952bee27b5051f48596f5157ac999d7a7792248", // arg: bytecode
 ]
---
+++ scripts/verify.sh
@@ -0,0 +1,5 @@
+yarn hardhat verify `
+--network bsc-testnet `
+--constructor-args .\config\bsc-testnet.argument.ts `
+--contract contracts/TokenProxy.sol:TokenProxy `
+0xa57D0e6B3f5Dde12A7b99ac347d4A5473BA3fdeb
